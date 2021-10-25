4aa400396229ed6da3b6134d13c6e8b6239f6032
AbdulrahmanY <abdulrahmansaddiq@gmail.com>
2021-10-25 15:20:19 +0300

    task 2

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.css';
-import CurrentTime from "./CurrentTime"
-import ButtonLink from "./ButtonLink.js"
+
 function App() {
-  let activities = ["playing football","watch movies","playing online games"]
+  let MenuList = {name:"Pasta",price:50,image:"https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2020%2F09%2F17%2Fcream-tomato-rigatoni-FT-RECIPE1020.jpg"}
   return (
     <div className="App">
-      
-      <ButtonLink>{ButtonLink}</ButtonLink>
-      <h2 id="h2"></h2>
-      <p>activities:{activities.map(function(ele,ind){
-        return " "+ele + ", "
-      })}</p>
+      <h1>name: {MenuList.name}</h1>
+      <p>Price: {MenuList.price}</p>
+      <img src={MenuList.image}/>
     </div>
   );
   
