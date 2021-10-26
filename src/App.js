77e83c9dc3a0badfecd1b2f640f23f109ac98518
FahadTariqiDev <fahad.tariqi17@gmail.com>
2021-10-26 14:00:29 +0300

    new version

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function Appp() {
+  const TWU = () => {
+    window.location = "https://tuwaiq.edu.sa/";
+  };
+  return (
+    <>
+      <button onClick={TWU}>Click me</button> 
+      <button onClick={TWU}>Click me</button>
+      <button onClick={TWU}>Click me</button>
+      <button onClick={TWU}>Click me</button>
+      <button onClick={TWU}>Click me</button>
+    
+
+    </>
+  );
+}
+
+export default Appp;
---
+++ src/List.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function list(prop) {
+    return (
+    <div>
+        
+      <ul>
+          <li>{prop.act1}</li>
+      </ul>
+    </div>
+  );
+}
+export default list;
